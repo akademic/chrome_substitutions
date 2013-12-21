e03dce9fbb1affc2ff7a0574771087a0b7f729b0
Akademic <public@akademic.name>
2013-12-21 22:17:48 +0400

    Get data for substitution by message mechanism

--- main_content.js
+++ main_content.js
@@ -1,27 +1,11 @@
-var dictionary = [
-{f: 'свидетели', t: 'знакомые чуваки'},
-{f: 'свидетелей', t: 'знакомых чуваков'},
-{f: 'предположительно', t: 'вроде как'},
-{f: 'новые исследования', t: 'пост на tumblr\'е'},
-{f: 'восстановить', t: 'отомстить за'},
-{f: 'смартфон', t: 'томагочи'},
-{f: 'электрический', t: 'атомный'},
-{f: 'депутат', t: 'эльфийский лорд'},
-{f: 'выборы', t: 'еда на скорость'},
-{f: 'лидеры партий', t: 'духи реки'},
-{f: 'лидер партии', t: 'дух реки'},
-{f: 'АНБ', t: 'КВН'},
-{f: 'отказался давать коментарии', t: 'виновен, и все прекрасно это знают'},
-{f: 'Единая Россия', t: 'Партия Жуликов и Воров'},
-]
-
 var getTextNodesIn = function(el) {
     return $(el).find(":not(iframe)").addBack().contents().filter(function() {
         return this.nodeType == 3;
     });
 };
 
-function substitute_text() {
+function substitute_text(dictionary) {
+    console.log(dictionary);
     var nodes = getTextNodesIn('html')
     $.each(nodes, function(nid, node) {
         var text = node.textContent
@@ -47,5 +31,13 @@ function subst_item(item, to, mod) {
     return mod
 }
 
-$(substitute_text)
+function _init() {
+    chrome.runtime.sendMessage({action: "get_dict"}, function(response) {
+        var dict = JSON.parse(response)
+        substitute_text(dict)
+    });
+}
+
+
+$(_init)
 
